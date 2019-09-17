675a66471ec8d6f11937e04e9b45afa8e9c98141
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2019-09-17 01:04:27 -0400

    commit

---
+++ Lab01/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab01.Models;
+using System.Web;
+
+namespace Lab01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        // GET: /Home/Addition/5
+        public string Addition(string x, string y)
+        {
+            string message = HttpUtility.HtmlEncode("Result = "+ x + y);
+
+            return message;
+        }
+
+        // GET: /Home/Multiplication/5
+        public string Multiplication(int x, int y)
+        {
+            string message = "Result = " + x * y ;
+
+            return message;
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
